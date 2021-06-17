f906cc545bea93d882ef7abf5783a8ded8d9e37d
ZwerOxotnik <zweroxotnik@gmail.com>
2021-06-17 16:32:57 +0300

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,6 +1,5 @@
 
--- Don't init if it's a campaign
-if script.level.campaign_name then return end
+if script.level.campaign_name then return end -- Don't init if it's a campaign
 if script.level.level_name == "sandbox" then return end -- Don't init if it's "sandbox" scenario
 
 require("defines")
