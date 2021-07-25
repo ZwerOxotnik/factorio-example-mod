6a5992658400f7b86c8ce76deb363432c10a5cb9
ZwerOxotnik <zweroxotnik@gmail.com>
2021-07-25 07:59:01 +0300

    Improve description

--- const-commands.lua
+++ const-commands.lua
@@ -5,13 +5,13 @@ Returns tables of commands without functions as command "settings". All paramete
   Contains:
     name :: string: The name of your /command. (default: key of the table)
     description :: string or LocalisedString: The description of your command. (default: nil)
-    is_allowed_empty_args :: bool: Ignores empty parameters in commands, otherwise stops the command. (default: true)
+    is_allowed_empty_args :: boolean: Ignores empty parameters in commands, otherwise stops the command. (default: true)
     input_type :: string: filter for parameters by type of input. (default: nil)
       possible variants:
         "player" - Stops execution if can't find a player by parameter
         "team" - Stops execution if can't find a team (force) by parameter
-    allow_for_server :: bool: Allow execution of a command from a server (default: false)
-    only_for_admin :: bool: The command can be executed only by admins (default: false)
+    allow_for_server :: boolean: Allow execution of a command from a server (default: false)
+    only_for_admin :: boolean: The command can be executed only by admins (default: false)
 ]]--
 ---@type table<string, table>
 return {}
