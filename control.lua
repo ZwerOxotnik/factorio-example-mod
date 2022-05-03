a0f117fd5b4557dc85fb6f64febcefb8c156e9e2
ZwerOxotnik <zweroxotnik@gmail.com>
2022-05-03 17:43:50 +0300

    Add compatibility with gvv

--- control.lua
+++ control.lua
@@ -41,3 +41,8 @@ else
 end
 
 event_handler.add_libraries(modules)
+
+
+-- This is a part of "gvv", "Lua API global Variable Viewer" mod. https://mods.factorio.com/mod/gvv
+-- It makes possible gvv mod to read sandboxed variables in the map or other mod if following code is inserted at the end of empty line of "control.lua" of each.
+if script.active_mods["gvv"] then require("__gvv__.gvv")() end
