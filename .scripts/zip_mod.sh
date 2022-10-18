d14fae6f960b8f64e0a3d7f699fa222b8a7a65b0
ZwerOxotnik <zweroxotnik@gmail.com>
2022-10-18 10:56:40 +0300

    Update zip_mod.sh

--- .scripts/zip_mod.sh
+++ .scripts/zip_mod.sh
@@ -7,18 +7,29 @@ REPOSITORY=`pwd`
 cd "$REPOSITORY/"
 
 
-### Get mod name and version from info.json
-### https://stedolan.github.io/jq/
+### Check commands
+has_errors=false
 if ! command -v jq &> /dev/null; then
   echo "Please install jq https://stedolan.github.io/jq/"
-  exit
+  has_errors=true
 fi
+if ! command -v 7z &> /dev/null; then
+  echo "Please install 7-Zip https://www.7-zip.org/download.html"
+fi
+if [ $has_errors = true ] ; then
+	exit
+fi
+
+
+### Get mod name and version from info.json
+### https://stedolan.github.io/jq/
 mod_name=`cat info.json|jq -r .name`
 mod_ver=`cat info.json|jq -r .version`
 
 
 ### Prepare zip for Factorio native use and mod portal
-name="${mod_name}_$mod_ver"
+### https://www.7-zip.org/download.html
+name="${mod_name}_${mod_ver}"
 git clean -xdf
 cd ..
-7z a -xr'!.*' "$REPOSITORY/$name.zip" "$REPOSITORY"
+7z a -xr'!.*' "${REPOSITORY}/${name}.zip" "${REPOSITORY}"
