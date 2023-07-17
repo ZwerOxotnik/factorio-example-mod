6d3c0524672fa1ffd0c95992baf6f0ec7d6dbffe
ZwerOxotnik <zweroxotnik@gmail.com>
2023-07-17 13:47:26 +0300

    Improve global defines

--- control.lua
+++ control.lua
@@ -2,6 +2,7 @@
 if script.level.campaign_name then return end -- Don't init if it's a campaign
 if script.level.level_name == "sandbox" then return end -- Don't init if it's "sandbox" scenario
 
+MAKE_DEFINE_GLOBAL = true
 require("defines")
 
 
--- defines.lua
+++ defines.lua
@@ -1,7 +1,18 @@
 -- Change everything in this file in your mod!
+local _data = {
+	MOD_NAME = "example-mod",
+	MOD_PATH = "__example-mod__",
 
-MOD_NAME = "example-mod"
-MOD_PATH = "__example-mod__"
+	-- Don't use symbols like '-' etc (it'll break pattern of regular expressions)
+	MOD_SHORT_NAME = "em_",
+	AUTHOR = "ZwerOxotnik"
+}
 
--- Don't use symbols like '-' etc (it'll break pattern of regular expressions)
-MOD_SHORT_NAME = "em_"
+if not MAKE_DEFINE_GLOBAL or (not IS_DATA_STAGE and script and script.active_mods) then
+	return _data
+end
+
+--- Make content of _data global
+for k, v in pairs(_data) do
+	_ENV[k] = v
+end
